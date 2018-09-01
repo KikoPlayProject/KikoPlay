7d95b8110b057d968b449c1d5f19d45ce8e7aaf6
Kikyou <dx_8820832@yeah.net>
2018-09-01 23:11:34 +0800

    catch unhandled exception

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include "UI/mainwindow.h"
 #include <QApplication>
+#include <Windows.h>
+#include <DbgHelp.h>
+#include <QMessageBox>
 #include "globalobjects.h"
-
+#pragma comment (lib,"DbgHelp.lib")
+LONG AppCrashHandler(EXCEPTION_POINTERS *pException) 
+{			
+	HANDLE hDumpFile = CreateFile((LPCWSTR)(QCoreApplication::applicationDirPath() + QDateTime::currentDateTime().toString("\\yyyy-MM-dd-hh-mm-ss")+".dmp").utf16(), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hDumpFile != INVALID_HANDLE_VALUE) 
+	{
+		MINIDUMP_EXCEPTION_INFORMATION dumpInfo;
+		dumpInfo.ExceptionPointers = pException;
+		dumpInfo.ThreadId = GetCurrentThreadId();
+		dumpInfo.ClientPointers = TRUE;
+		MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), hDumpFile, MiniDumpNormal, &dumpInfo, NULL, NULL);
+	}
+	EXCEPTION_RECORD* record = pException->ExceptionRecord;
+	QString errCode(QString::number(record->ExceptionCode, 16)), errAdr(QString::number((uint)record->ExceptionAddress, 16)), errMod;
+	QMessageBox::critical(nullptr, "Error", QString("Error Code: %1 Error Address: %2").arg(errCode).arg(errAdr),QMessageBox::Ok);
+	return EXCEPTION_EXECUTE_HANDLER;
+}
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+	SetUnhandledExceptionFilter((LPTOP_LEVEL_EXCEPTION_FILTER)AppCrashHandler);
     QString qss;
     QFile qssFile(":/res/style.qss");
     qssFile.open(QFile::ReadOnly);
@@ -19,7 +39,6 @@ int main(int argc, char *argv[])
     QTranslator translator;
     if(translator.load(trans))
         a.installTranslator(&translator);
-
     GlobalObjects::init();
     MainWindow w;
     w.show();
