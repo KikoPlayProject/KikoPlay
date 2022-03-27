d036af6ac6fde873e11b04e04836de5a81af4b6e
Kikyou <dx_8820832@yeah.net>
2022-03-28 01:04:11 +0800

    update tip

--- UI/tip.cpp
+++ UI/tip.cpp
@@ -2,17 +2,20 @@
 #include <QLabel>
 #include <QVBoxLayout>
 #include <QFile>
+#include <QApplication>
 Tip::Tip(QWidget *parent) : CFramelessDialog (tr("Tip"),parent)
 {
     QLabel *tipContent=new QLabel(this);
-    tipContent->setTextInteractionFlags(Qt::TextSelectableByMouse);
+    tipContent->setTextInteractionFlags(Qt::TextBrowserInteraction);
     tipContent->setWordWrap(true);
     tipContent->setOpenExternalLinks(true);
     QFile tipFile(":/res/tip");
     tipFile.open(QFile::ReadOnly);
     if(tipFile.isOpen())
     {
-        tipContent->setText(tipFile.readAll());
+        QString tipText = tipFile.readAll();
+        tipText = tipText.replace("{AppPath}", QCoreApplication::applicationDirPath());
+        tipContent->setText(tipText);
     }
     QVBoxLayout *tipVLayout=new QVBoxLayout(this);
     tipVLayout->addWidget(tipContent);
